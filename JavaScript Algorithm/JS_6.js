77be83293636b36b3c429501330240afec9750aa
dahlia <wlstnrdahlia@gmail.com>
2023-03-08 16:15:46 +0900

    add : 문제6 False 문제풀이 업로드

---
+++ JavaScript Algorithm/JS_6.js
@@ -0,0 +1,13 @@
+
+// False
+// 다음은 자바스크립트 문법 중에서 False로 취급하는 것들 입니다.
+// 앗, False로 취급하지 않는 것이 하나 있네요! True를 찾아주세요.
+
+// null, undefined, 0, 빈 문자열, NaN, false는 False 나머지는 True
+NaN // False
+1 // True
+"" // 빈문자열 False
+0 // 0은 False
+undefined // False
+
+// 2번 1이 정답
