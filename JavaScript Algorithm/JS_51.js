d6aec95a27edd5341d0a59ccba53468b66bc6934
dahlia <wlstnrdahlia@gmail.com>
2023-05-18 23:52:06 +0900

    add : JS 51 문제풀이 업로드

---
+++ JavaScript Algorithm/JS_51.js
@@ -0,0 +1,38 @@
+
+// merge sort 병합 정렬
+
+function mergeSort(arr){
+  if (arr.length <= 1){
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0,mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right){
+  let result = [];
+
+  while (left.length && right.length){
+    if (left[0] < right[0]){
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  while (left.length) {
+    result.push(left.shift());
+  }
+  while (right.length) {
+    result.push(right.shift());
+  }
+
+  return result;
+}
+
+const array = prompt('배열을 입력하세요').split(' ').map(n => parseInt(n, 10));
+
+console.log(mergeSort(array));
