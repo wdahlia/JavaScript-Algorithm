366b4508728333c252a2d5cbc21a8c93b63622ca
dahlia <108653518+wdahlia@users.noreply.github.com>
2024-01-14 10:45:24 +0900

    [level Lv.0] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers, n) {
+    let answer = 0;
+    for (number of numbers) {
+        if (answer > n) {
+            return answer
+        } else {
+            answer += number
+        }
+    }
+    return answer;
+}
