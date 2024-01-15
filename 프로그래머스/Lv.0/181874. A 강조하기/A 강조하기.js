8fdd871f93a61c7b438e90159b233c634b195260
dahlia <108653518+wdahlia@users.noreply.github.com>
2024-01-15 14:16:47 +0900

    [level Lv.0] Title: A 강조하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/Lv.0/181874. A 강조하기/A 강조하기.js
@@ -1,9 +1,7 @@
 function solution(myString) {
     // myString a만 대문자로 a 아닌건 전부다 소문자로;
-    let answer = '';
-    answer = myString.replace(/a/g, 'A');
-    answer = answer.replace(/[B-Z]/g, function(match) {
-        return match.toLowerCase();
+    let answer = myString.replace(/[aB-Z]/g, function(match) {
+        return match === 'a' ? 'A' : match.toLowerCase();
     });
     return answer;
 }
