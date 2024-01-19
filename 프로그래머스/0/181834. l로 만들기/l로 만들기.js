42565c6d6b67fe058c744c831f9b60f2b4b40dfe
dahlia <108653518+wdahlia@users.noreply.github.com>
2024-01-19 17:15:13 +0900

    [level 0] Title: l로 만들기, Time: 7.20 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,6 +1,4 @@
 function solution(myString) {
-    let answer = '';
-    const value = 'l'.charCodeAt();
-    answer = myString.replaceAll(/[a-zA-Z]/g, str => str.charCodeAt() < value ? str = 'l' : str ) 
+    var answer = '';
     return answer;
 }
