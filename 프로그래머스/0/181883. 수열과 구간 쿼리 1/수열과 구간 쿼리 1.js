a30130378dd059e95fa0eb34e338419888debb3b
dahlia <108653518+wdahlia@users.noreply.github.com>
2024-01-22 15:21:37 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 5.72 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,10 +1,21 @@
 function solution(arr, queries) {
     // queries 내부에 들어있는 [s, e]에서 s보다 크거나 같고 e보다 작거나 같은 index에 대해 arr[i]에 1을 더함
     let answer = [...arr];
+
+    // for문 사용
+    // for ([s, e] of queries) {
+    //     for (let i=s; i<=e; i++) {
+    //         answer[i] += 1
+    //     }
+    // }
+    
+    // filter 사용
     for ([s, e] of queries) {
-        for (let i=s; i<=e; i++) {
-            answer[i] += 1
-        }
+        answer.filter((item, i) => {
+            if (s <= i && i <= e) {
+                answer[i] += 1
+            }
+        })
     }
     
     return answer;
